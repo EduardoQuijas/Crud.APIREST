78e6c450f9fb3f5f387f52d1564b85d0861a4c58
EduardoQuijas <143649410+EduardoQuijas@users.noreply.github.com>
2023-08-30 12:56:54 -0600

    Add files via upload

---
+++ CrudrapidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Crudrapido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudrapidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
